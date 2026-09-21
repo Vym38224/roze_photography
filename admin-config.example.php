bf6919f49f1f22f8ad244581e6277a3547787279
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2026-09-21 14:03:08 +0200

    popis k configu

--- admin-config.example.php
+++ admin-config.example.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    // Preferujte hash vytvoreny funkci password_hash().
+    // Toto je jen příklad jak vypadá originální admin-config.php bez hesla, originální soubor je pro bezpečnost schován
     'password_hash' => 'PASTE_PASSWORD_HASH_HERE',
 ];
